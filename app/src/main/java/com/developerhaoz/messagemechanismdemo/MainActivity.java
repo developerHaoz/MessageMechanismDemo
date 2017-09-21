15eef5d4c795ee8d410bb502530da17adf628c37
Haoz <developerhaon@gmail.com>
2017-09-21 21:07:12 +0800

    写了常见的 Handler 用法

--- app/src/main/java/com/developerhaoz/messagemechanismdemo/MainActivity.java
+++ app/src/main/java/com/developerhaoz/messagemechanismdemo/MainActivity.java
@@ -1,13 +1,67 @@
 package com.developerhaoz.messagemechanismdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private TextView mTvShowInfo;
+    private Handler handler = new Handler(){
+
+        @Override
+        public void handleMessage(Message msg) {
+            if(msg.what == 1){
+                String result = msg.obj.toString();
+                mTvShowInfo.setText(result);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTvShowInfo = (TextView) findViewById(R.id.main_tv_show_info);
+        final String url = "http://gank.io/api/data/%E7%A6%8F%E5%88%A9/10/48";
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url(url)
+                        .build();
+
+                try {
+                    final Response response = okHttpClient.newCall(request).execute();
+                    final String result = response.body().string();
+                    Message message = Message.obtain();
+                    message.what = 1;
+                    message.obj = result;
+                    handler.sendMessage(message);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(null);
     }
 }
